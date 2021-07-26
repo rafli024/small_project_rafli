e2f926583ddae655c9420d1b3beb27f2bba2e637
rafli024 <raleigh4460@gmail.com>
2021-07-26 15:02:22 +0000

    little changes

--- script_unduh_web.sh
+++ script_unduh_web.sh
@@ -2,15 +2,15 @@
 echo "download Landing page"
 wget -O landing-page.zip  https://github.com/sdcilsy/landing-page/archive/refs/heads/master.zip
 echo "extracting compressed file to destination directory"
-sudo unzip landing-page.zip -d /var/www/html/
+sudo unzip landing-page.zip -d /var/www/
 echo "download wordpress site"
 wget -O wordpress-id.tar.gz https://id.wordpress.org/latest-id_ID.tar.gz
 echo "extracting to /var/www/html"
-sudo tar -xzvf wordpress-id.tar.gz -C /var/www/html
+sudo tar -xzvf wordpress-id.tar.gz -C /var/www/
 echo "download social media site"
 wget -O social-media.zip https://github.com/sdcilsy/sosial-media/archive/refs/heads/master.zip
 echo "extracting social media site to /var/www/html"
-sudo unzip social-media.zip -d /var/www/html/
+sudo unzip social-media.zip -d /var/www/
 echo "creating table for social media site"
 sudo mysqldump -u root -p /var/www/html/social-media-master/dump.sql > social_media.sql
 echo "done"
