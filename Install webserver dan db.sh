7441402cfef613018c0b2bbcba378bb7065ab3f1
rafli024 <raleigh4460@gmail.com>
2021-07-26 17:51:46 +0700

    adding multiple choice

--- Install webserver dan db.sh
+++ Install webserver dan db.sh
@@ -1,13 +1,24 @@
-#!/bin/sh
-
-jawaban_webserver = ("Y" "y")
-read -p "apakah anda ingin menginstall webserver? (Y/n)" install_apache2;
-if [$jawaban_webserver == $install_apache2]
-then
-    echo "melakukan installasi webserver"
-    sudo apt-get install apache2
-    read -p "apakah anda ingin menginstall database? (Y/n)"
-else
-
-    
-fi
+#!/bin/bash
+PS3='Pilih paket instalasi yang diinginkan : '
+install=("Webserver" "Database" "Quit")
+select fav in "${install[@]}"; do
+    case $fav in
+        "Webserver")
+            echo "Installing $fav"
+            # melakukan instalasi apache2 webserver
+            echo "installing apache2 as $fav"
+            sudo apt-get install apache2
+            ;;
+        "Database")
+            echo "Installing $fav"
+	        # melakukan instalasi Mysql database
+            echo "installing mysql as $fav"
+	    break
+            ;;
+	"Quit")
+	    echo "User requested exit"
+	    exit
+	    ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
