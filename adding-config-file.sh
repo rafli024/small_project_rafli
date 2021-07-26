4a97ac60ac86bc36ccef391346f2632af4aafab8
rafli024 <raleigh4460@gmail.com>
2021-07-26 15:35:50 +0000

    adding new command

--- adding-config-file.sh
+++ adding-config-file.sh
@@ -1,1 +1,29 @@
 #!/bin/sh
+sudo cat > landing_page.conf << EOF >/etc/apache2/sites-available/landing_page.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/landing-page-master
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+sudo cat > wordpress.conf << EOF >/etc/apache2/sites-available/wordpress.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/wordpress
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+sudo cat > social-media.conf << EOF >/etc/apache2/sites-available/social-media.conf
+<VirtualHost *:80>
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/social-media-master
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+</VirtualHost>
+EOF
+
+
