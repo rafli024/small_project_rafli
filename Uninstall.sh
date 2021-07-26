0f25393cbc0f6b5775d7aba93f9c27634bb83158
rafli024 <raleigh4460@gmail.com>
2021-07-26 16:30:06 +0000

    fixing prompt

--- Uninstall.sh
+++ Uninstall.sh
@@ -2,6 +2,6 @@
 sudo mysql -u root -p -e "drop database sosial_media"
 sudo apt  remove apache2* -y
 sudo apt remove mysql-server* -y
-sudo apt purge apache2*
-sudo apt purge mysql-server*
-sudo apt autoclean
+sudo apt purge apache2* -y
+sudo apt purge mysql-server* -y
+sudo apt auto-clean
