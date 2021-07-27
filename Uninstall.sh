896988b55af58a7457ce4beac9959692de7d6433
rafli024 <raleigh4460@gmail.com>
2021-07-27 07:46:57 +0000

    deleting unnecessary script and making backup

--- Uninstall.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo mysql -u root -p -e "drop database sosial_media"
-sudo apt  remove apache2* -y
-sudo apt remove mysql-server* -y
-sudo apt purge apache2* -y
-sudo apt purge mysql-server* -y
-sudo apt auto-clean
