108ed124b334c211add47498e8c60e83ffdf943e
rafli024 <raleigh4460@gmail.com>
2021-07-30 08:47:37 +0000

    removing sudo command

--- install_webserver_and_database.sh
+++ install_webserver_and_database.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "anda perlu menjalankan script ini menggunakan root, jika tidak pilih 5 dan masuk kembali sebagai root"
 PS3='Pilih Opsi : '
 install=("Webserver" "Database" "Download web file" "Uninstall" "Quit")
 select fav in "${install[@]}"; do
@@ -7,51 +8,57 @@ select fav in "${install[@]}"; do
             echo "Installing $fav"
             # melakukan instalasi apache2 webserver
             echo "installing apache2 as $fav"
-            sudo apt-get install apache2 -y
+            apt-get install apache2 -y
+            apt-get install php php-mysql -y
+            service apache2 restart
             ;;
         "Database")
             echo "Installing $fav"
 	        # melakukan instalasi Mysql database
             echo "installing mysql as $fav"
-            sudo apt-get install mysql-server -y
+            apt-get install mysql-server -y
             ;;
         "Download web file")
             echo "download $fav"
+            # proses instalasi landing-page ke dalam webserver
             wget -O landing-page.zip  https://github.com/sdcilsy/landing-page/archive/refs/heads/master.zip
             echo "extracting compressed file to destination directory"
-            sudo unzip landing-page.zip -d /var/www/html
+            unzip landing-page.zip -d /var/www/html
+            # proses instalasi wordpress ke dalam webserver
             echo "download wordpress site"
             wget -O wordpress-id.tar.gz https://id.wordpress.org/latest-id_ID.tar.gz
             echo "extracting content to /var/www/html"
-            sudo tar -xzvf wordpress-id.tar.gz -C /var/www/html
+            tar -xzvf wordpress-id.tar.gz -C /var/www/html
+            echo "configure wordpress"
+            
+             mysql -u root -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+            mysql -u root -p -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '1234567890';"
+            mysql -u root -p -e "GRANT ALL PRIVILEGES ON * . * TO 'wordpress'@'localhost';"
+            mysql -u root -p -e "FLUSH PRIVILEGES;"
             echo "download social media site"
             wget -O social_media.zip https://github.com/sdcilsy/sosial-media/archive/refs/heads/master.zip
             echo "extracting social media site to /var/www/html"
-            sudo unzip social-media.zip -d /var/www/html
+            unzip social-media.zip -d /var/www/html
             echo "copy wordpress conf file"
-            sudo cp wordpress.conf /etc/apache2/sites-available/wordpress.conf
+            cp wordpress.conf /etc/apache2/sites-available/wordpress.conf
             echo "configuring database"
-            sudo mysql -u root -e "CREATE DATABASE wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
-            sudo mysql -u root -p -e "CREATE USER 'wordpress'@'localhost' IDENTIFIED BY '1234567890';"
-            sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON * . * TO 'wordpress'@'localhost';"
-            sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
             echo "creating dbusername and dbpassword"
-            sudo mysql -u root -p -e "CREATE USER 'devopscilsy'@'localhost' IDENTIFIED BY '1234567890';"
-            sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON * . * TO 'devopscilsy'@'localhost';"
-            sudo mysql -u root -p -e "FLUSH PRIVILEGES;"
+            mysql -u root -p -e "CREATE USER 'devopscilsy'@'localhost' IDENTIFIED BY '1234567890';"
+            mysql -u root -p -e "GRANT ALL PRIVILEGES ON * . * TO 'devopscilsy'@'localhost';"
+            mysql -u root -p -e "FLUSH PRIVILEGES;"
             echo "creating database for social media site"
-            sudo mysql -u root -p -e "create database dbsosmed"
-            sudo mysql -u devopscilsy -p dbsosmed < /var/www/html/sosial-media-master/dump.sql
+            mysql -u root -p -e "create database dbsosmed"
+            mysql -u devopscilsy -p dbsosmed < /var/www/html/sosial-media-master/dump.sql
             echo "done"
             ;;
         "Uninstall")
-            sudo mysql -u root -p -e "drop database dbsosmed"
-            sudo apt  remove apache2* -y
-            sudo apt remove mysql-server* -y
-            sudo apt purge apache2* -y
-            sudo apt purge mysql-server* -y
-            sudo apt auto-clean
-            sudo rm -rf /var/*
+            mysql -u root -p -e "drop database dbsosmed"
+            apt  remove apache2* -y
+            apt remove mysql-server* -y
+            apt purge apache2* -y
+            apt purge mysql-server* -y
+            apt auto-clean
+            rm -rf /var/*
 	    break
             ;;
 	"Quit")
